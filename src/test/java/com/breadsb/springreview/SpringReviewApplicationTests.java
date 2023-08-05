abe41e4fdc859a9389e44c14ea75d52cb4c70b42
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-08-05 14:59:01 +0200

    first commit

---
+++ src/test/java/com/breadsb/springreview/SpringReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breadsb.springreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
