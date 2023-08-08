fc0521aaed07d31fe3c52967fee4f5e304bf53d2
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-08-08 17:40:28 +0200

    build.gradle school module

---
+++ school/src/main/java/com/breadsb/school/SchoolSpringApplication.java
@@ -0,0 +1,11 @@
+package com.breadsb.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolSpringApplication.class,args);
+    }
+}
