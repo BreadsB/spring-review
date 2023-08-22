ab19bb69ff77b1f11ba876097b26d95218b8899a
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-08-22 17:17:03 +0200

    test update note

--- notes/src/test/java/com/breadsb/services/NoteServiceTest.java
+++ notes/src/test/java/com/breadsb/services/NoteServiceTest.java
@@ -4,6 +4,7 @@ import com.breadsb.entities.Note;
 import com.breadsb.exceptions.ResourceNotFoundException;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,30 +12,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 
 @SpringBootTest
+@Transactional
 class NoteServiceTest {
 
     @Autowired
     NoteService service;
 
+    Note note;
+
     @Test
-    @Transactional
     void givenNote_whenGetById_thenReceiveNote() {
         //given
-        Note note1 = new Note("Test title1", "Test body1");
+        note = new Note("Test title1", "Test body1");
         //when
-        service.createNote(note1);
+        service.createNote(note);
         //then
-        Long id = note1.getId();
+        Long id = note.getId();
         Note receivedNote = service.getNoteById(id);
         Assertions.assertNotNull(receivedNote);
         Assertions.assertEquals("Test title1", receivedNote.getTitle());
         Assertions.assertEquals(receivedNote.getCreatedAt().getDayOfYear(), LocalDateTime.now().getDayOfYear());
     }
 
+    @BeforeEach
+    void setUp() {
+        note = new Note("My test note", "Something in body");
+    }
+
     @Test
-    @Transactional
     void testDeleteNoteFromDb() {
-        Note note = new Note("My test note", "Something in body");
+
         service.createNote(note);
         long id = note.getId();
 
@@ -45,5 +52,15 @@ class NoteServiceTest {
         });
     }
 
+    @Test
+    void testUpdateNote() {
+        service.createNote(note);
+        long id = note.getId();
+        Note updateNote = new Note("Updated title", "Updated body");
+        service.updateNote(id,updateNote);
 
+        Note retrieve = service.getNoteById(id);
+        Assertions.assertEquals("Updated title", retrieve.getTitle());
+        Assertions.assertEquals("Updated body", retrieve.getBody());
+    }
 }
