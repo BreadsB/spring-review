919909927d93352e4a5f5eefb178807c3b2b9284
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-08-26 00:17:22 +0200

    html and script changed

--- notes/src/main/java/com/breadsb/front/script.js
+++ notes/src/main/java/com/breadsb/front/script.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-    var inputTitle = $('#inputTitle');
-    var inputText = $('#inputText');
+    var noteForm = $('[data-note-add-form]')
     var sendButton = $('#sendButton');
     var clearButton = $('#clearButton');
-    var apiRoot = "http://localhost:8080/notes/api/notes/";
+    var apiRoot = "http://localhost:8080/api/notes/";
 
     function postData(event) {
         event.preventDefault();
-        
-        var inputTitleValue = $(this).find('[id="inputTitle"]').val();
-        var inputTextValue = $(this).find('[id="inputText"]').val();
+
+        var inputTitleValue = $('#inputTitle').val();
+        var inputTextValue = $('#inputText').val();
         var requestUrl = apiRoot;
-        
+
+        console.log(inputTitleValue);
+        console.log(inputTextValue);
+
         $.ajax({
             url: requestUrl,
-            processData: false,
             method: 'POST',
+            processData: false,
             contentType: "application/json; charset=utf-8",
             dataType: 'json',
             data: JSON.stringify({
@@ -33,11 +35,6 @@ $(document).ready(function () {
         });
     }
 
-    sendButton.on('click', function () {
-        sendData();
-    });
+    noteForm.on('submit', postData);
 
-    clearButton.on('click', function () {
-        inputText.val('');
-    });
 });
